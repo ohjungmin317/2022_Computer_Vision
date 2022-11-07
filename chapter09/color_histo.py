95dc6fd7e84e4795cd14edc6c72bd74be79b7b01
오정민 <90691262+ohjungmin317@users.noreply.github.com>
2022-11-07 22:25:35 +0900

    Computervision_10week

--- chapter09/color_histo.py
+++ chapter09/color_histo.py
@@ -1,26 +1,26 @@
 import numpy as np
 import cv2 as cv
 
-def color_histo():
-    src = cv.imread('pepper.bmp',cv.IMREAD_COLOR)
+src = cv.imread('pepper.bmp')
 
-    if src is None:
-        print('Image load failed')
-        return
+if src is None:
+    print('Image load failed')
+    exit()
 
-    src_ycrcb = cv.cvtColor(src,cv.COLOR_BGR2YCrCb)
+src_ycrcb = cv.cvtColor(src, cv.COLOR_BGR2YCrCb)
+ycrcb_planes = cv.split(src_ycrcb)
 
-    ycrcb_planes = cv.split(src_ycrcb)
+# cv.imshow('y',ycrcb_planes[0])
+# cv.waitKey()
 
-    ycrcb_planes[0] = cv.equalizeHist(ycrcb_planes[0])
+ycrcb_planes = np.asarray(ycrcb_planes)
 
-    dst_ycrcb = cv.merge(ycrcb_planes)
+ycrcb_planes[0] = cv.equalizeHist(ycrcb_planes[0])
 
-    dst = cv.cvtColor(dst_ycrcb, cv.COLOR_YCrCb2BGR)
+dst_ycrcb = cv.merge(ycrcb_planes)
+dst = cv.cvtColor(dst_ycrcb, cv.COLOR_YCrCb2BGR)
 
-    cv.imshow('src', src)
-    cv.imshow('dst', dst)
-    cv.waitKey()
-    cv.destroyAllWindows()
-
-color_histo()
+cv.imshow('src',src)
+cv.imshow('dst',dst)
+cv.waitKey()
+cv.destroyAllWindows()
